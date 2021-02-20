a69147ea995afbec8d8735e11b09787f55fd0848
Linh <lhvo2112@gmail.com>
2021-02-20 11:12:55 -0500

    Added components in profile panel

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,14 +2,27 @@ import React from 'react';
 import classes from './Dashboard.module.css';
 import profile_pic from '../assets/profile.png';
 
+import {Github} from './icons/Github';
+import {Linkedin} from './icons/Linkedin';
+import {Email} from './icons/Email';
+import {ThemeLight} from './icons/ThemeLight';
+
+
 export const Dashboard = () => {
   return (
     <div className={classes.container}>
       <div className={classes.profile}>
+
         <img className={classes.profilePic} src={profile_pic} alt="an illustration of myself" width="130px" height="130px" />
         <p>Linh Vo</p>
 
-        <div className={classes.mode}></div>
+        <div className={classes.iconContainer}>
+          <Github />
+          <Linkedin />
+          <Email />
+        </div>
+
+        <ThemeLight />
       </div>
     </div>
 
