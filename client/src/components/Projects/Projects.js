ce2cef3ac2680965f6cd79ec0b637d1e94b69df2
Linh <lhvo2112@gmail.com>
2021-02-27 18:26:57 -0500

    Added custom scrollbar

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import classes from './Projects.module.css';
-
+import {ScrollBar} from '../UI/ScrollBar/ScrollBar';
 import {Header} from './Header/Header';
 import {Project} from './Project';
 
 export const Projects = () => {
     return (
-        <div className={classes.projectsContainer}>
-            <Header />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <div className={classes.lastRow}></div>
-        </div>
+        <ScrollBar>
+            <div className={classes.projectsContainer}>
+                <Header />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <div className={classes.lastRow}></div>
+            </div>
+        </ScrollBar>
 
     );
 };
---
+++ client/src/components/UI/ScrollBar/ScrollBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Scrollbars} from 'rc-scrollbars';
+
+export const ScrollBar = (props) => {
+
+    const thumbVertical = ({style, ...props}) => {
+        const finalStyle = {
+            ...style,
+            backgroundColor: 'rgba(127, 148, 185, 0.53)',
+            width: '7px'
+        };
+        return <div style={finalStyle} {...props} />;
+    };
+
+
+    return (
+        <Scrollbars
+            style={{height: '90vh'}}
+            renderThumbVertical={thumbVertical}
+        >
+            {props.children}
+        </Scrollbars>
+    );
+};
