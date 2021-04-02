78517bc5e922d52604f7f90e984a7506a661b172
Linh <lhvo2112@gmail.com>
2021-04-02 17:05:08 -0400

    Fixed background gradient colors

--- client/src/components/Background.js
+++ client/src/components/Background.js
@@ -2,13 +2,10 @@ import React from 'react';
 import classes from './Background.module.css';
 
 export const Background = () => {
-  return (
-    <div className={classes.container}>
-      <div className={classes.ellipse1}></div>
-      <div className={classes.ellipse2}></div>
-      <div className={classes.ellipse3}></div>
-      <div className={classes.ellipse4}></div>
-      <div className={classes.ellipse5}></div>
-    </div>
-  );
+    return (
+        <div className={classes.container}>
+            <div className={classes.ellipse1}></div>
+            <div className={classes.ellipse2}></div>
+        </div>
+    );
 };
