0e6c13e89acdb3fb20dd4b37cecd2015e09fe727
Linh <lhvo2112@gmail.com>
2021-04-12 10:29:34 -0400

    Added links to social icons

--- client/src/components/ProfilePanel/Profile.js
+++ client/src/components/ProfilePanel/Profile.js
@@ -18,10 +18,10 @@ export const Profile = () => {
                 <img className={classes.profilePic} src={profile_pic} alt="an illustration of myself" />
                 <p className={classes.name}>Linh Vo</p>
                 <div className={classes.iconContainer}>
-                    <Social name='Github' />
-                    <Social name='Linkedin' />
-                    <Social name='Email' />
-                    <Social name='Resume' />
+                    <Social name='Github' href='https://www.github.com/linhhvo' />
+                    <Social name='Linkedin' href='https://www.linkedin.com/in/linhhvo' />
+                    <Social name='Email' href='mailto: lhvo2112@gmail.com' />
+                    <Social name='Resume' href='https://drive.google.com/file/d/1QQF0A6JmqEmBn-WC_Z4gAGgBQRfM2Tv2/view?usp=sharing' />
                 </div>
             </div>
 
--- client/src/components/ProfilePanel/Social.js
+++ client/src/components/ProfilePanel/Social.js
@@ -2,12 +2,12 @@ import React from 'react';
 import classes from './Social.module.css';
 
 export const Social = (props) => {
-  return (
-    <div className={classes.container}>
-      <svg>
-        <use href={`#${props.name}`} />
-      </svg>
-      <p>{props.name}</p>
-    </div>
-  );
+    return (
+        <div className={classes.container} onClick={() => window.open(props.href)}>
+            <svg>
+                <use href={`#${props.name}`} />
+            </svg>
+            <p>{props.name}</p>
+        </div>
+    );
 };
