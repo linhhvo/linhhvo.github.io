1e8399dd6da31de9a9b31389dfdc100bc98134df
Linh <lhvo2112@gmail.com>
2021-04-30 18:15:40 -0400

    Populate list of skills to filter from projects

--- client/src/components/Projects/Header/FilterBar/FilterList.js
+++ client/src/components/Projects/Header/FilterBar/FilterList.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import classes from './FilterList.module.css';
+import {GlobalContext} from '../../../../context/GlobalState';
 
 export const FilterList = (props) => {
+    const {projects} = useContext(GlobalContext);
+
+    let skills = new Set();
+
+    projects.map(project => {
+        project.skill.map(skill => skills.add(skill));
+    });
+
     let styleClass = props.expandList ? `${classes.container}` : `${classes.container} ${classes.collapsed}`;
 
     return (
         <div className={styleClass}>
             <ul>
-                <li>Show All</li>
-                <li>React</li>
-                <li>MongoDB</li>
-                <li>NodeJS</li>
-                <li>ExpressJS</li>
-                <li>Python</li>
-                <li>Bootstrap</li>
+                {[...skills].sort().map(skill => <li key={skill}>{skill}</li>)}
             </ul>
         </div>
     );
