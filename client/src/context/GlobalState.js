a424c20887d3306761acd50279c62022a2587e70
Linh <lhvo2112@gmail.com>
2021-04-29 21:57:54 -0400

    Fixed theme switch function

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -48,7 +48,7 @@ export const GlobalProvider = ({children}) => {
                 document.getElementById('main').classList.replace('dark', 'light');
             }
         }
-    }, []);
+    });
 
     // Switch theme function to allow toggle theme with light bulb icon
     const switchTheme = () => {
