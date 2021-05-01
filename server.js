d754df3a4252d1b7cf33bdd934fda155b9deee33
Linh <lhvo2112@gmail.com>
2021-05-01 18:33:48 -0400

    Added package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/database');
+const path = require('path');
 
 // Get global env variable
 dotenv.config({path: './config/config.env'});
