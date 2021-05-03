a9d86b5267e253417c9849e6528b234d922594b7
Linh <lhvo2112@gmail.com>
2021-05-03 11:03:23 -0400

    Chnaged colors in light mode

--- client/src/components/UI/ScrollBar/ScrollBar.js
+++ client/src/components/UI/ScrollBar/ScrollBar.js
@@ -3,21 +3,11 @@ import {Scrollbars} from 'rc-scrollbars';
 import css from './ScrollBar.module.css';
 
 export const ScrollBar = (props) => {
-
-    const thumbVertical = ({style, ...props}) => {
-        const finalStyle = {
-            ...style,
-            backgroundColor: 'rgba(127, 148, 185, 0.53)',
-            width: '7px'
-        };
-        return <div style={finalStyle} {...props} />;
-    };
-
     return (
         <Scrollbars
-            // style={{height: '90vh'}}
-            classes={{root: css.root}}
-            renderThumbVertical={thumbVertical}
+            classes={{
+                root: css.root
+            }}
             autoHide
         >
             {props.children}
